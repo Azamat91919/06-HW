e2c392020b9c8933c2d57ad38240c1a9285ada97
Azamat91919 <116088049+Azamat91919@users.noreply.github.com>
2022-11-16 16:03:37 +0600

    Add files via upload

---
+++ 06/index.js
@@ -0,0 +1,18 @@
+let numbers = [1,2,41,5,25,55,22,55].map(item=> item**2);
+console.log(numbers);
+
+let array = [33,12,331,35,25,55,22,55].filter(item=> item <35);
+console.log(array)
+
+let currentWord = ["reactjs", "vuejs", "angularjs", "nodejs"].reduce(function(longest, currentWord) {
+    if(currentWord.length > longest.length)
+       return currentWord;
+    else
+       return longest;
+  });
+
+  console.log(currentWord)
+
+let array1 = ["reactjs", "vuejs", "angularjs", "nodejs"].map(sweetString => { return sweetString.toUpperCase()})
+console.log(array1)
+
